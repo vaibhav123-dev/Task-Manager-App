213964cc420007e8b27effc2f41a5471eb4fd954
Vaibhav Darvekar <vabdarwekar00@gmail.com>
2024-09-12 22:49:41 +0530

    update

--- client/src/common/apiRequest.js
+++ client/src/common/apiRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://task-manager-app-fgga.vercel.app/api",
+  baseURL: "http://localhost:8000/api",
   headers: {
     "Content-Type": "application/json",
   },
