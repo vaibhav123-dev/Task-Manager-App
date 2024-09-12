50613e3d95e632db17a7e757b48754a3d13ddd2c
Vaibhav Darvekar <vabdarwekar00@gmail.com>
2024-09-12 16:34:40 +0530

    update

--- server/src/app.js
+++ server/src/app.js
@@ -26,6 +26,18 @@ app.use(
   })
 );
 
+if (process.env.NODE_ENV === "production") {
+  const path = require("path");
+
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  // Serve index.html for any routes that don't match your API
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 app.use(express.static("public"));
 
 app.use(cookieParser());
