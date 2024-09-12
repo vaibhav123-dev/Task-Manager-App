32e1b8ab67fd89f2b8cd88038e936c85c21512aa
Vaibhav Darvekar <vabdarwekar00@gmail.com>
2024-09-12 16:55:55 +0530

    update

--- server/src/middlewares/multerMiddleware.js
+++ server/src/middlewares/multerMiddleware.js
@@ -4,7 +4,7 @@ import multer from "multer";
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "./public/temp");
+    cb(null, "./public");
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
